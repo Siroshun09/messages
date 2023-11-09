51aebb09929512b3e532c1f31fbc2d43d5774aef
Siroshun09 <siro.shun8@gmail.com>
2023-11-10 02:23:26 +0900

    docs(api): add package-info.java for directory package

---
+++ api/src/main/java/com/github/siroshun09/messages/api/directory/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A package that includes classes for loading {@link com.github.siroshun09.messages.api.source.MessageSource}s from the directory.
+ */
+package com.github.siroshun09.messages.api.directory;
