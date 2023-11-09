10a71cfa1904a57ae320093f16d77fa423048ec9
Siroshun09 <siro.shun8@gmail.com>
2023-11-10 02:24:27 +0900

    build: setup aggregate javadoc task

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.aggregate.javadoc)
+    alias(libs.plugins.javadoc.links)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    javadoc(projects.messagesApi)
+
+    javadocClasspath(libs.annotations)
+    javadocClasspath(libs.adventure.api)
+    javadocClasspath(libs.adventure.minimessage)
+    javadocClasspath(libs.adventure.serializer.legacy)
+}
+
+tasks {
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+        (options as StandardJavadocDocletOptions)
+                .docTitle("Messages $version")
+                .windowTitle("Messages $version")
+                .bottom("<p class=\"legalCopy\">Copyright &#169;2023 <a href=\"https://github.com/Siroshun09\">Siroshun09</a>.</p>")
+    }
+}
