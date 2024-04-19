42e7c274ef8b5cb12a1133d58aeb39d028364ad1
Siroshun09 <siro.shun8@gmail.com>
2024-04-19 19:43:12 +0900

    clean(api): remove MessageMap#mergeAndCollectMissingMessages

--- api/src/main/java/com/github/siroshun09/messages/api/source/MessageMap.java
+++ api/src/main/java/com/github/siroshun09/messages/api/source/MessageMap.java
@@ -1,6 +1,5 @@
 package com.github.siroshun09.messages.api.source;
 
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
@@ -43,19 +42,6 @@ public interface MessageMap<T> extends MessageSource<T> {
      * @return the messages that were added to this {@link MessageMap}
      */
     @NotNull @Unmodifiable Map<String, T> merge(@NotNull Map<String, T> other);
-
-    /**
-     * Merges messages from the given {@link Map} and collect missing messages.
-     *
-     * @param other the {@link Map} to merge
-     * @return the messages that were not included in this {@link MessageMap}
-     * @deprecated use {@link #merge(Map)} instead of this method
-     */
-    @Deprecated(forRemoval = true, since = "0.6.0")
-    @ApiStatus.ScheduledForRemoval(inVersion = "0.7.0")
-    default @NotNull @Unmodifiable Map<String, T> mergeAndCollectMissingMessages(@NotNull Map<String, T> other) {
-        return this.merge(other);
-    }
 
     /**
      * Returns the unmodifiable message map.
