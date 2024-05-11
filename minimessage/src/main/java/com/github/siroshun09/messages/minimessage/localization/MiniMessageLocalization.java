12f639f0227c7889619578502dfdf8b09ac39152
Siroshun09 <siro.shun8@gmail.com>
2024-05-12 08:12:37 +0900

    feat(minimessage): add MiniMessageLocalization#toTranslator

--- minimessage/src/main/java/com/github/siroshun09/messages/minimessage/localization/MiniMessageLocalization.java
+++ minimessage/src/main/java/com/github/siroshun09/messages/minimessage/localization/MiniMessageLocalization.java
@@ -4,10 +4,22 @@ import com.github.siroshun09.messages.api.localize.AbstractLocalization;
 import com.github.siroshun09.messages.api.localize.Localization;
 import com.github.siroshun09.messages.minimessage.source.FallingBackMiniMessageSource;
 import com.github.siroshun09.messages.minimessage.source.MiniMessageSource;
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.minimessage.Context;
+import net.kyori.adventure.text.minimessage.ParsingException;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.translation.Translator;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.text.MessageFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.function.Function;
@@ -49,5 +61,85 @@ public class MiniMessageLocalization extends AbstractLocalization<Component, Min
     public @NotNull FallingBackMiniMessageSource findSource(@Nullable Object obj) {
         var locale = this.localeGetter.apply(obj);
         return locale != null ? this.findSource(locale) : new FallingBackMiniMessageSource(defaultSource(), defaultSource());
+    }
+
+    /**
+     * Creates a {@link Translator} that uses this {@link MiniMessageLocalization}.
+     * <p>
+     * Arguments of {@link TranslatableComponent} can be obtained by argument tags (e.g. <code>&lt;argument:0&gt;</code>, <code>&lt;arg:1&gt;</code>).
+     *
+     * @param key the {@link Key} for returning by {@link Translator#name()}
+     * @return a {@link Translator}
+     */
+    // See https://github.com/KyoriPowered/adventure/pull/972
+    @ApiStatus.Experimental
+    public @NotNull Translator toTranslator(@NotNull Key key) {
+        return new TranslatorImpl(key, this);
+    }
+
+    private record TranslatorImpl(@NotNull Key name,
+                                  @NotNull MiniMessageLocalization localization) implements Translator {
+
+        private static final boolean HAS_ARGUMENTS_METHOD;
+
+        static {
+            boolean hasArgumentsMethod;
+
+            try {
+                TranslatableComponent.class.getDeclaredMethod("arguments");
+                hasArgumentsMethod = true;
+            } catch (NoSuchMethodException ignored) {
+                hasArgumentsMethod = false;
+            }
+
+            HAS_ARGUMENTS_METHOD = hasArgumentsMethod;
+        }
+
+        @SuppressWarnings("deprecation")
+        private static @NotNull List<? extends ComponentLike> getArguments(@NotNull TranslatableComponent component) {
+            return HAS_ARGUMENTS_METHOD ? component.arguments() : component.args();
+        }
+
+        @Override
+        public @Nullable MessageFormat translate(@NotNull String key, @NotNull Locale locale) {
+            var source = this.localization.findSource(locale);
+            return source.hasMessage(key) ? new MessageFormat(source.getRawMessage(key), locale) : null;
+        }
+
+        @Override
+        public @Nullable Component translate(@NotNull TranslatableComponent component, @NotNull Locale locale) {
+            var source = this.localization.findSource(locale);
+            var key = component.key();
+            return source.hasMessage(key) ? this.render(source, key, component) : null;
+        }
+
+        private Component render(@NotNull MiniMessageSource source, @NotNull String key, @NotNull TranslatableComponent component) {
+            var arguments = getArguments(component);
+            var result = arguments.isEmpty() ? source.getMessage(key) : source.getMessage(key, new ArgumentTag(arguments));
+
+            return component.children().isEmpty() ? result : result.children(component.children());
+        }
+
+        private record ArgumentTag(List<? extends ComponentLike> arguments) implements TagResolver {
+
+            private static final String ARGUMENT_NAME = "argument";
+            private static final String ARG_NAME = "arg";
+
+            @Override
+            public @NotNull Tag resolve(final @NotNull String name, final @NotNull ArgumentQueue arguments, final @NotNull Context ctx) throws ParsingException {
+                final int index = arguments.popOr("No argument number provided").asInt().orElseThrow(() -> ctx.newException("Invalid argument number", arguments));
+
+                if (index < 0 || index >= this.arguments.size()) {
+                    throw ctx.newException("Invalid argument number", arguments);
+                }
+
+                return Tag.inserting(this.arguments.get(index));
+            }
+
+            @Override
+            public boolean has(final @NotNull String name) {
+                return name.equals(ARGUMENT_NAME) || name.equals(ARG_NAME);
+            }
+        }
     }
 }
--- minimessage/src/test/java/com/github/siroshun09/messages/minimessage/localization/MiniMessageLocalizationTest.java
+++ minimessage/src/test/java/com/github/siroshun09/messages/minimessage/localization/MiniMessageLocalizationTest.java
@@ -4,12 +4,17 @@ import com.github.siroshun09.messages.api.localize.Localization;
 import com.github.siroshun09.messages.api.source.StringMessageMap;
 import com.github.siroshun09.messages.minimessage.source.MiniMessageSource;
 import com.github.siroshun09.messages.test.shared.localization.AbstractLocalizationTest;
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @SuppressWarnings("DataFlowIssue")
 class MiniMessageLocalizationTest extends AbstractLocalizationTest<Component, MiniMessageSource> {
@@ -39,5 +44,18 @@ class MiniMessageLocalizationTest extends AbstractLocalizationTest<Component, Mi
     @Override
     protected @NotNull MiniMessageSource createSource() {
         return MiniMessageSource.create(StringMessageMap.create());
+    }
+
+    @Test
+    void testTranslator() {
+        var key = "test";
+        var format = "<aqua>Arguments: <argument:0>, <arg:1>";
+        var children = List.of(Component.text("Additional", NamedTextColor.RED), Component.text("Component", NamedTextColor.WHITE, TextDecoration.BOLD));
+        var component = Component.translatable(key, Component.text(1), Component.text(2)).children(children);
+        var localization = new MiniMessageLocalization(MiniMessageSource.create(StringMessageMap.create(Map.of(key, format))));
+
+        var expected = Component.text("Arguments: 1, 2", NamedTextColor.AQUA).children(children);
+        var translated = localization.toTranslator(Key.key("test")).translate(component, Locale.getDefault());
+        Assertions.assertEquals(expected, translated);
     }
 }
